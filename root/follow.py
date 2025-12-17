b98f2b95533e6190e27161b536b0e779a499a0da
Natnael Samson <Natnaelsamson41@gmail.com>
2025-12-17 17:29:33 +0300

    Implement FOLLOW set calculation

---
+++ root/follow.py
@@ -0,0 +1,62 @@
+from first import first
+
+def follow(grammar):
+    FIRST = first(grammar)
+    FOLLOW = {}
+
+    for i in grammar:
+        values = i.split("->")
+        FOLLOW[values[0].strip()] = []
+
+    start_symbol = grammar[0].split("->")[0].strip()
+    FOLLOW[start_symbol].append('$')
+
+    has_changed = True
+    while has_changed:
+        has_changed = False
+
+        for i in grammar:
+            left, right = i.split("->")
+            left = left.strip()
+            right = right.strip()
+
+            for j in grammar:
+                j_left, j_right = j.split("->")
+                j_left = j_left.strip()
+                j_right = j_right.strip()
+
+                symbols = j_right.split()
+
+                if left in symbols:
+                    location = symbols.index(left)
+
+                    if location + 1 < len(symbols):
+                        next_symbol = symbols[location + 1]
+
+                        if not next_symbol.isupper():
+                            if next_symbol not in FOLLOW[left]:
+                                FOLLOW[left].append(next_symbol)
+                                has_changed = True
+
+                        else:
+                            for k in FIRST[next_symbol]:
+                                if k != 'e' and k not in FOLLOW[left]:
+                                    FOLLOW[left].append(k)
+                                    has_changed = True
+
+                            if 'e' in FIRST[next_symbol]:
+                                if location + 2 == len(symbols):
+                                    for f in FOLLOW[j_left]:
+                                        if f not in FOLLOW[left]:
+                                            FOLLOW[left].append(f)
+                                            has_changed = True
+
+                    else:
+                        if left != j_left:
+                            for f in FOLLOW[j_left]:
+                                if f not in FOLLOW[left]:
+                                    FOLLOW[left].append(f)
+                                    has_changed = True
+
+    return FOLLOW
+
